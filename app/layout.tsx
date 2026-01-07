f3a3a6d2fa2dd78c6834049a1c00727f28bd89d2
Michael Primavera <mr.michael.primavera@outlook.com>
2026-01-07 15:58:34 -0500

    more text adjustments

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MikhaelRobotics™",
+  title: "Michael Primavera",
   description: "Startup robotics compnay",
 };
 
