f567084f5c69001be0aa0107cf50b0e08b4ad784
Michael Primavera <mr.michael.primavera@outlook.com>
2026-01-27 17:14:11 -0500

    fixed grid_item componenet

--- app/components/grid_item.tsx
+++ app/components/grid_item.tsx
@@ -28,13 +28,13 @@ export const GridItem = ({
         />
 
         {/* Icon + Title */}
-        <div className="flex items-center justify-center gap-3 hover:-translate-y-0.5 transition-all
-           duration-300">
+        <div className="flex items-center justify-center gap-3 min-w-0
+          hover:-translate-y-0.5 transition-all duration-300">
           <div className="flex-shrink-0">{icon}</div>
           <h3 className="
             text-sm lg:text-xl md:text-lg sm:text-sl
             font-semibold text-blue-400 text-center
-            break-words whitespace-normal">
+            break-normal whitespace-normal">
             {title}
           </h3>
         </div>
