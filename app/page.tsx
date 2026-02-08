55a48452d072cdf4f33158f4676ac0970f475e1d
Michael Primavera <mr.michael.primavera@outlook.com>
2026-02-08 14:56:28 -0500

    updated main page title

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
           w-full max-w-3xl
           ">
           <div className="border rounded-3xl text-slate-400/50 mb-16 pt-4 backdrop-blur-sm bg-black/35 min-w-[410px] text-center">
-            <ProjectSection title="Robotics Engineer & Software Developer">
+            <ProjectSection title="Robotics & Software Engineering Portfolio">
               <div className="w-full max-w-7xl mx-auto px-4">
                 <p className="text-base sm:text-xl text-slate-500 pt-4 mb-6">
                   This site showcases my production-quality software engineering work through real projects, with additional
