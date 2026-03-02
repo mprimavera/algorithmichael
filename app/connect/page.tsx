9695b93f0d4f9649570bb2a7f3fa27ce73e2bb1f
Michael Primavera <mr.michaelprimavera@outlook.com>
2026-03-02 17:20:30 -0500

    mobile device testing

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -12,7 +12,7 @@ export default function ConnectPage() {
     <div className="relative w-full min-h-screen flex items-center justify-center px-4 md:px-8 lg:px-10">
 
       {/* Background vortex */}
-      <div className="absolute inset-0 flex items-center justify-center -z-10">
+      <div className="absolute inset-0 -z-10">
         <Vortex baseHue={180} />
       </div>
       
